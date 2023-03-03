416040a2ccfd5015fefdd39c7271611c47b9a7f5
Ranjith4024 <kumar.ranjith.srk@gmail.com>
2023-03-03 09:53:31 +0530

    adding

---
+++ main.js
@@ -0,0 +1,220 @@
+let data=[
+    {
+      img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/rtd7prrar0swhfjokao3',
+      hotelname:'SS Hydrebad Biriyani',
+      cusin:'hydrabad, south Indian',
+      rate:'<iconify-icon icon="ic:round-star"></iconify-icon>4.9',
+      time:'25min',
+      price:'Rs250 For Two',
+      offer:'<iconify-icon class="icon" icon="bxs:offer"></iconify-icon> 50% off | Use WELCOME50',
+      ext:'<a href="">Quick View</a>',
+    },
+    {      img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ny8kj6b4mltw140wil1l',
+      hotelname:'new jum jum',
+      cusin:'hydrabad, south Indian',
+      rate:'<iconify-icon icon="ic:round-star"></iconify-icon>4.9',
+      time:'25min',
+      price:'Rs250 For Two',
+      offer:'<iconify-icon class="icon" icon="bxs:offer"></iconify-icon> 50% off | Use WELCOME50',
+      ext:'<a href="">Quick View</a>',
+    },
+    {      img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/gk0ulc71jckzqx0z5sdk',
+    hotelname:'JK Mess',
+    cusin:' south Indian',
+    rate:'<iconify-icon icon="ic:round-star"></iconify-icon>4.5',
+    time:'15min',
+    price:'Rs150 For Two',
+    offer:'<iconify-icon class="icon" icon="bxs:offer"></iconify-icon> 50% off | Use WELCOME50',
+    ext:'<a href="">Quick View</a>',
+  },
+  {      img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ac1staxtetolkvynv2ot',
+  hotelname:'Zaitton',
+  cusin:'hydrabad,',
+  rate:'<iconify-icon icon="ic:round-star"></iconify-icon>4.3',
+  time:'35min',
+  price:'Rs250 For Two',
+  offer:'<iconify-icon class="icon" icon="bxs:offer"></iconify-icon> 50% off | Use WELCOME50',
+  ext:'<a href="">Quick View</a>',
+},
+{      img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ny8kj6b4mltw140wil1l',
+hotelname:'new jum jum',
+cusin:'Indian',
+rate:'<iconify-icon icon="ic:round-star"></iconify-icon>4.9',
+time:'25min',
+price:'Rs250 For Two',
+offer:'<iconify-icon class="icon" icon="bxs:offer"></iconify-icon> 50% off | Use WELCOME50',
+ext:'<a href="">Quick View</a>',
+},
+{      img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/affnh0fjqay8bvxyovi8',
+hotelname:'Arabian Grills',
+cusin:'Arabian',
+rate:'<iconify-icon icon="ic:round-star"></iconify-icon>4.9',
+time:'45min',
+price:'Rs350 For Two',
+offer:'<iconify-icon class="icon" icon="bxs:offer"></iconify-icon> 50% off | Use WELCOME50',
+ext:'<a href="">Quick View</a>',
+},
+{      img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/snuiscl9oddarseimjz0',
+hotelname:'Salem RR Briyani',
+cusin:' South Indian',
+rate:'<iconify-icon icon="ic:round-star"></iconify-icon>4.9',
+time:'35min',
+price:'Rs250 For Two',
+offer:'<iconify-icon class="icon" icon="bxs:offer"></iconify-icon> 50% off | Use WELCOME50',
+ext:'<a href="">Quick View</a>',
+},
+{      img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ycbupye6drimplzlucqx',
+hotelname:'Chithamram tr Briyani',
+cusin:'Indian',
+rate:'<iconify-icon icon="ic:round-star"></iconify-icon>4.9',
+time:'25min',
+price:'Rs250 For Two',
+offer:'<iconify-icon class="icon" icon="bxs:offer"></iconify-icon> 50% off | Use WELCOME50',
+ext:'<a href="">Quick View</a>',
+},
+{      img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/pruhipbrdrcc8cczugdi',
+hotelname:'Muslim briyani',
+cusin:'South Indian',
+rate:'<iconify-icon icon="ic:round-star"></iconify-icon>4.9',
+time:'25min',
+price:'Rs250 For Two',
+offer:'<iconify-icon class="icon" icon="bxs:offer"></iconify-icon> 50% off | Use WELCOME50',
+ext:'<a href="">Quick View</a>',
+},
+{      img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ny8kj6b4mltw140wil1l',
+hotelname:'new jum jum',
+cusin:'Indian',
+rate:'<iconify-icon icon="ic:round-star"></iconify-icon>4.9',
+time:'25min',
+price:'Rs250 For Two',
+offer:'<iconify-icon class="icon" icon="bxs:offer"></iconify-icon> 50% off | Use WELCOME50',
+ext:'<a href="">Quick View</a>',
+},
+{      img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ny8kj6b4mltw140wil1l',
+hotelname:'new jum jum',
+cusin:'Indian',
+rate:'<iconify-icon icon="ic:round-star"></iconify-icon>4.9',
+time:'25min',
+price:'Rs250 For Two',
+offer:'<iconify-icon class="icon" icon="bxs:offer"></iconify-icon> 50% off | Use WELCOME50',
+ext:'<a href="">Quick View</a>',
+},
+{      img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ny8kj6b4mltw140wil1l',
+hotelname:'new jum jum',
+cusin:'Indian',
+rate:'<iconify-icon icon="ic:round-star"></iconify-icon>4.9',
+time:'25min',
+price:'Rs250 For Two',
+offer:'<iconify-icon class="icon" icon="bxs:offer"></iconify-icon> 50% off | Use WELCOME50',
+ext:'<a href="">Quick View</a>',
+},
+{      img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ny8kj6b4mltw140wil1l',
+hotelname:'new jum jum',
+cusin:'Indian',
+rate:'<iconify-icon icon="ic:round-star"></iconify-icon>4.9',
+time:'25min',
+price:'Rs250 For Two',
+offer:'<iconify-icon class="icon" icon="bxs:offer"></iconify-icon> 50% off | Use WELCOME50',
+ext:'<a href="">Quick View</a>',
+},
+{      img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ny8kj6b4mltw140wil1l',
+hotelname:'new jum jum',
+cusin:'Indian',
+rate:'<iconify-icon icon="ic:round-star"></iconify-icon>4.9',
+time:'25min',
+price:'Rs250 For Two',
+offer:'<iconify-icon class="icon" icon="bxs:offer"></iconify-icon> 50% off | Use WELCOME50',
+ext:'<a href="">Quick View</a>',
+},
+{      img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ny8kj6b4mltw140wil1l',
+hotelname:'new jum jum',
+cusin:'Indian',
+rate:'<iconify-icon icon="ic:round-star"></iconify-icon>4.9',
+time:'25min',
+price:'Rs250 For Two',
+offer:'<iconify-icon class="icon" icon="bxs:offer"></iconify-icon> 50% off | Use WELCOME50',
+ext:'<a href="">Quick View</a>',
+},
+{      img:'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/ny8kj6b4mltw140wil1l',
+hotelname:'new jum jum',
+cusin:'Indian',
+rate:'<iconify-icon icon="ic:round-star"></iconify-icon>4.9',
+time:'25min',
+price:'Rs250 For Two',
+offer:'<iconify-icon class="icon" icon="bxs:offer"></iconify-icon> 50% off | Use WELCOME50',
+ext:'<a href="">Quick View</a>',
+},
+]
+
+
+
+let sec = document.createElement('section');
+document.body.appendChild(sec);
+
+let container = document.createElement("div");
+sec.appendChild(container);
+container.classList.add('container');
+let row = document.createElement('div');
+container.appendChild(row);
+row.classList.add('row');
+
+
+data.forEach(function(raj){
+
+
+
+let col = document.createElement('div');
+row.appendChild(col);
+col.classList.add('col');
+
+let card = document.createElement('div');
+col.appendChild(card);
+card.classList.add('card');
+
+let img= document.createElement('img');
+img.src=raj.img;
+img.classList.add('image');
+card.appendChild(img);
+
+let head= document.createElement('h3');
+head.classList.add('head');
+head.innerHTML=raj.hotelname;
+card.appendChild(head);
+
+let para= document.createElement('p');
+para.classList.add('para');
+para.innerHTML=raj.cusin;
+card.appendChild(para);
+
+let div= document.createElement('div');
+div.classList.add('content');
+
+card.appendChild(div);
+
+let para1= document.createElement('p');
+para1.classList.add('para1');
+para1.innerHTML=raj.rate;
+div.appendChild(para1);
+
+let para2= document.createElement('p');
+para2.classList.add('para2');
+para2.innerHTML=raj.time;
+div.appendChild(para2);
+
+let para3= document.createElement('p');
+para3.classList.add('para3');
+para3.innerHTML=raj.price;
+div.appendChild(para3);
+
+let div1= document.createElement('div');
+div1.classList.add('content1');
+div1.innerHTML=raj.offer;
+card.appendChild(div1);
+
+let div2= document.createElement('div');
+div2.classList.add('div2');
+div2.innerHTML=raj.ext;
+card.appendChild(div2);
+
+
+});
